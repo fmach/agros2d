8bfe8ff5866cd05e83d802c027406d060963d004
František Mach <mach.frantisek@gmail.com>
2014-10-14 21:30:26 +0200

    variant model refractored

--- resources/python/variant/model.py
+++ resources/python/variant/model.py
@@ -1,12 +1,13 @@
 _empty_svg = '<?xml version="1.0" encoding="UTF-8" standalone="no"?><svg xmlns="http://www.w3.org/2000/svg" version="1.0" width="32" height="32" viewBox="0 0 32 32"></svg>'
 
+import abc
 import pickle
 import os
-import warnings
 
 class StrictDict(dict):
-    """General class for model data (parameters and variables).
-       Class is inherited from dict and implement data type checking."""
+    """Dictionary for model data.
+    Class is inherited from `dict` and implement data type checking.
+    """
 
     def __init__(self, *args, **kwargs):
         self.update(*args, **kwargs)
@@ -14,52 +15,88 @@ class StrictDict(dict):
 
     @property
     def data_types(self):
+        """Return declared items."""
         return self._data_types
 
+    def declare(self):
+        raise NotImplementedError()
+
     def fulfilled(self):
+        """Check that all declared items (`self.data_types`) are defined.
+        :returns: bool -- Return True if all items are defined otherwise return False.
+        """
+
         for key in self._data_types.keys():
             if not key in self.keys(): return False
         return True
 
-    def __setitem__(self, key, value):
-        if not key in self._data_types:
-            raise KeyError('Value with name "{0}" is not declared!'.format(key))
+    def __setitem__(self, name, value):
+        if not name in self._data_types:
+            raise KeyError('Item with name "{0}" is not declared!'.format(name))
 
-        data_type = self._data_types[key]
+        data_type = self._data_types[name]
         if ((type(value) != data_type) and
             (not (data_type == float and type(value) == int))):
-            raise TypeError('Value data type do not correspond with defined data type!')
+            raise TypeError('Data type of value do not correspond with defined data type!')
 
-        self.update({key : value})
+        self.update({name : value})
 
 class Parameters(StrictDict):
-    def declare(self, key, data_type, default=None):
+    """Dictionary for model input parameters.
+    Class is inherited from :class:`StrictDict` and implement default values of declared items.
+    """
+
+    def declare(self, name, data_type, default=None):
+        """Declare new model input parameter.
+        :param name: Name of new parameter.
+        :type name: str
+        :param data_type: Data type of parameter.
+        :type data_type: type
+        :param default: Default value for parameter (default is None).
+        :type default: Data type depands on `data_type`.
+        """
+
         if default:
             if ((type(default) != data_type) and
                 (not (data_type == float and type(default) == int))):
                 raise TypeError('Value data type do not correspond with defined data type!')
-            self.update({key : default})
+            self.update({name : default})
 
-        self._data_types[key] = data_type
+        self._data_types[name] = data_type
 
 class Variables(StrictDict):
-    def declare(self, key, data_type):
-        self._data_types[key] = data_type
+    """Dictionary for model output variables.
+    Class is inherited from :class:`StrictDict`.
+    """
+
+    def declare(self, name, data_type):
+        """Declare new model output variable.
+        :param name: Name of new variable.
+        :type name: str
+        :param data_type: Data type of variable.
+        :type data_type: type
+        """
+        self._data_types[name] = data_type
 
 class ModelData(object):
-    """General class collected all model data."""
+    """Class collected all model data.
+    Class attribute `parameters` keeps input parameters, attribute `variables` output variables of model.
+    Both attributes must be instance of class inherited from :class:`StrictDict`.
+    Attribute `info` serve as repository for all other data.
+    """
 
-    def __init__(self):
+    def __init__(self, model_class):
         self.parameters = Parameters()
         self.variables = Variables()
 
         self.info = {'_geometry' : _empty_svg}
+        self.model_class = model_class
 
-class ModelBase(object):
-    """General class described model."""
+class ModelBase(metaclass=abc.ABCMeta):
+    """Abstract base class for description of the model."""
 
     def __init__(self):
-        self._data = ModelData()
+        self._data = ModelData(type(self))
         self.declare()
 
     @property
@@ -100,44 +137,51 @@ class ModelBase(object):
 
     @property
     def solved(self):
-        """Solution state of the model (return True if all declared variables are defined)."""
+        """Solution state of the model.
+        :returns: bool -- Return True if all declared variables are defined otherwise return False.
+        """
+
         return self._data.variables.fulfilled()
 
     @solved.setter
     def solved(self, value):
+        """Clear all output variables."""
         if not value: self._data.variables.clear()
 
+    @abc.abstractmethod
     def declare(self):
-        """Method declare model input parameters and ouput variables by ModelInfo class."""
-        warnings.warn("Method declare() should be overrided!", RuntimeWarning)
+        """Abstract method for declaration of model input parameters and ouput variables.
+        Method is called in class constructor and must be redefine in inhereted class.
+        For declaration of new parameter use :func:`self.parameters.declare` and for variable :func:`self.variables.declare`.
+        """
+        pass
 
     def create(self):
-        """Method creates model from input parameters."""
-        warnings.warn("Method create() should be overrided!", RuntimeWarning)
+        """Method for creation of model from input parameters.
+        Method is called by other OptiLab modules and should be redefine in inhereted class
+        """
+        pass
 
+    @abc.abstractmethod
     def solve(self):
-        """Method for solution of model."""
-        warnings.warn("Method solve() should be overrided!", RuntimeWarning)
+        """Abstract method for solution of model and calculation of variables.
+        Method must be redefine in inhereted class.
+        """
+        pass
 
     def load(self, file_name):
-        """Unpickle model and load binary file (marshalling of model object).
-
-        load(file_name)
-
-        Keyword arguments:
-        file_name -- file name of binary file for read
+        """Unpickle and load model data from binary file (marshalling of model object).
+        :param file_name: The file name of binary file with pickled data.
+        :type file_name: str
         """
 
         with open(file_name, 'rb') as infile:
             self._data = pickle.load(infile)
 
     def save(self, file_name):
-        """Pickle model and save binary file (serialization of model object).
-
-        save(file_name)
-
-        Keyword arguments:
-        file_name -- file name of binary file for write
+        """Pickle and save model data to binary file (serialization of model object).
+        :param file_name: The file name of binary file for pickled data.
+        :type file_name: str
         """
 
         directory = os.path.dirname(file_name)
@@ -148,13 +192,16 @@ class ModelBase(object):
             pickle.dump(self._data, outfile, 0) #pickle.HIGHEST_PROTOCOL
 
     def serialize(self):
-        """Serialize and return model data to string."""
+        """Serialize model data to string.
+        :returns: str -- Return string with serialized instance of :class:`ModelData` (:func:`self.data`).
+        """
+
         return pickle.dumps(self._data, 0) #pickle.HIGHEST_PROTOCOL
 
     def deserialize(self, data):
-        """Deserialize and return model data object from string.
-        
-        deserialize(data)
+        """Deserialize and set model data from string.
+        :param data: String for deserializaion.
+        :type data: str
         """
 
         self._data = pickle.loads(data)
@@ -167,10 +214,12 @@ if __name__ == '__main__':
             self.parameters.declare('p', float, default = 1.0)
             self.variables.declare('v', float)
 
+        def solve(self):
+            pass
+
     model = Model()
     model.parameters['p'] = 2.0
     model.variables['v'] = 2.0
-    print(model.solved)
 
     file_name = '{0}/model.pickle'.format(pythonlab.tempname())
     model.save(file_name)
--- resources/test/test_suite/optilab/model.py
+++ resources/test/test_suite/optilab/model.py
@@ -5,72 +5,103 @@ from test_suite.scenario import Agros2DTestResult
 
 from variant import ModelBase
 
-class VectorMultiplyingByScalar(ModelBase):
-    def declare(self):
-        self.declare_parameter('u', list)
-        self.declare_parameter('c', float, default=2.0)
-        self.declare_variable('v', list)
-
-    def create(self):
-        pass
-
-    def solve(self):
-        self.v = []
-        for ui in self.get_parameter('u'):
-            self.v.append(ui * self.get_parameter('c'))
-
-    def process(self):
-        self.set_variable('v', self.v)
-
 class Model(ModelBase):
     def declare(self):
-        self.model_info.add_parameter('p', int)
+        self.parameters.declare('u', list)
+        self.parameters.declare('c', float, default=2.0)
+        self.variables.declare('v', list)
+
+    def solve(self):
+        self.variables['v'] = []
+        for ui in self.parameters['u']:
+            self.variables['v'].append(ui * self.parameters['c'])
 
 class TestModel(Agros2DTestCase):
     def test_undeclared_parameter(self):
-        model = ModelBase()
+        model = Model()
         with self.assertRaises(KeyError):
-            model.set_parameter('undeclared_parameter', 1.0)
+            model.parameters['undeclared_parameter'] = 1.0
 
     def test_undeclared_variable(self):
-        model = ModelBase()
+        model = Model()
         with self.assertRaises(KeyError):
-            model.set_variable('undeclared_variable', 1.0)
+            model.variables['undeclared_variable'] = 1.0
 
     def test_parameter_with_bad_data_type(self):
-        model = VectorMultiplyingByScalar()
+        model = Model()
 
         with self.assertRaises(TypeError):
-            model.set_parameter('u', 1)
-
+            model.parameters['u'] = 1
         with self.assertRaises(TypeError):
-            model.set_parameter('u', {'u1' : 3, 'u2' : 5})
-
+            model.parameters['u'] = {'u1' : 3, 'u2' : 5}
         with self.assertRaises(TypeError):
-            model.set_parameter('u', (3, 5))
-
+            model.parameters['u'] = (3, 5)
         with self.assertRaises(TypeError):
-            model.set_parameter('c', [3, 5])
+            model.parameters['c'] = [3, 5]
+
+        try:
+            model.parameters['c'] = 1
+            model.parameters['c'] = 1.0
+        except:
+            self.assertFalse(True, 'Exception raised')
 
     def test_default(self):
-        model = VectorMultiplyingByScalar()
-        model.set_parameter('u', [5, 3, 6])
+        model = Model()
+        model.parameters['u'] = [5, 3, 6]
         model.solve()
-        model.process()
-        self.assertEqual([10, 6, 12], model.get_variable('v'))
+        self.assertEqual([10, 6, 12], model.variables['v'])
+
+    def test_rewrite_parameters(self):
+        model = Model()
+        with self.assertRaises(RuntimeError):
+            model.parameters = {'p' : 0.0}
+
+    def test_rewrite_variables(self):
+        model = Model()
+        with self.assertRaises(RuntimeError):
+            model.variables = {'v' : 0.0}
+
+    def test_rewrite_model_data(self):
+        model = Model()
+        with self.assertRaises(RuntimeError):
+            model.data = dict()
 
     def test_save_and_load(self):
-        file_name = '{0}/model.pickle'.format('{0}'.format(pythonlab.tempname()))
+        file_name = pythonlab.tempname('.pickle')
 
-        model = VectorMultiplyingByScalar()
-        model.set_parameter('u', [3, 5])
-        model.set_parameter('c', 3)
-        model.create()
+        model = Model()
+        model.parameters['u'] = [3, 5]
+        model.parameters['c'] = 3
         model.solve()
-        model.process()
         model.save(file_name)
         model.load(file_name)
-        self.assertEqual([9, 15], model.get_variable('v'))
+        self.assertEqual([9, 15], model.variables['v'])
+
+    def test_solved(self):
+        model = Model()
+        model.parameters['u'] = [1, 2, 3, 5]
+        model.solve()
+        self.assertTrue(model.solved)
+
+        model.solved = False
+        self.assertFalse(model.solved)
+        self.assertTrue('v' in model.variables.data_types.keys())
+        self.assertFalse('v' in model.variables.keys())
+
+    def test_model_class(self):
+        model = Model()
+        self.assertEqual(Model, model.data.model_class)
+
+    def test_serialize_and_deserialize(self):
+        model = Model()
+        model.parameters['u'] = [2, 3]
+        model.parameters['c'] = 5
+        model.solve()
+
+        pickled_data = model.serialize()
+        model.solved = False
+        model.deserialize(pickled_data)
+        self.assertTrue(model.solved)
 
 if __name__ == '__main__':
     import unittest as ut
